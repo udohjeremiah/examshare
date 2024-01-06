02f8d248a3b95392eab77ba5bcfcd4dc19f6cf2e
Udoh Jeremiah <udohjeremiah@icloud.com>
2024-01-06 20:57:54 +0100

    Add past exam question WDU/mkt202

--- src/app/past-questions-archive/wdu/mgmsci/2022_2023/200-level/2nd-semester/bus202/page.jsx
+++ src/app/past-questions-archive/wdu/mgmsci/2022_2023/200-level/2nd-semester/bus202/page.jsx
@@ -1,7 +1,7 @@
 import Questions from "@/components/Questions";
 import Answers from "@/components/Answers";
 
-export default function BUS102() {
+export default function BUS202() {
   return (
     <Questions
       school="western delta university"
--- src/app/past-questions-archive/wdu/mgmsci/2022_2023/200-level/2nd-semester/page.jsx
+++ src/app/past-questions-archive/wdu/mgmsci/2022_2023/200-level/2nd-semester/page.jsx
@@ -6,6 +6,10 @@ const courses = {
       name: "BUS 202 - Principles of Management II",
       route: "2nd-semester/bus202",
     },
+    {
+      name: "MKT 202 - Principles of Marketing",
+      route: "2nd-semester/mkt202",
+    },
   ],
 };
 
